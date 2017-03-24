d57c34bae6f78ef6036a14cb58c8581f940f4cf3
luka712 <lukaa712@hotmail.com>
2017-03-24 23:57:04 +0100

    add muzzle flash effect

---
+++ Assets/_scripts/MuzzleFlash/MuzzleFlash.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Animation that fadoes out overtime
+/// </summary>
+public class MuzzleFlash : MonoBehaviour
+{
+
+    [SerializeField]
+    private float fadeOutSpeed = 3f;
+
+    /// <summary>
+    /// X range.
+    /// </summary>
+    public Vector2 XRange = new Vector2(.6f, 1f);
+
+    /// <summary>
+    /// Y range
+    /// </summary>
+    public Vector2 YRange = new Vector2(.6f, 1f);
+
+    /// <summary>
+    /// Rotation offset.
+    /// </summary>
+    public Vector2 MinAndMaxRotation = new Vector2(-10f, 10f); 
+
+    private SpriteRenderer renderer;
+    private bool play;
+    private Color initialColor;
+    private Quaternion initialRotation;
+
+    /// <summary>
+    /// Unity start
+    /// </summary>
+	private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        initialColor = renderer.color;
+        initialRotation = this.transform.rotation;
+    }
+
+    /// <summary>
+    /// Unity update
+    /// </summary>
+    private void Update()
+    {
+        if (play)
+        {
+            initialColor.a -= Time.deltaTime * fadeOutSpeed;
+            renderer.color = initialColor;
+            if (initialColor.a <= 0f)
+            {
+                play = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Plays animation.
+    /// </summary>
+    public void Play(bool applyDirectionChange = false)
+    {
+        this.transform.localScale
+            = new Vector3(Random.Range(XRange.x, XRange.y), Random.Range(YRange.x, YRange.y), 1f);
+        initialColor.a = 1f;
+        renderer.color = initialColor;
+
+        this.transform.rotation = initialRotation;
+
+        if(applyDirectionChange)
+        {
+            this.transform.Rotate(0, 0, 180);
+        }
+
+        this.transform.Rotate(0, 0, Random.Range(MinAndMaxRotation.x, MinAndMaxRotation.y));
+
+        play = true;
+    }
+}
--- Assets/_scripts/Weapons/MachineGun.cs
+++ Assets/_scripts/Weapons/MachineGun.cs
@@ -11,8 +11,18 @@ public class MachineGun : BaseWeapon
 
     [SerializeField]
     private GameObject bulletPrefab;
+    [SerializeField]
+    private GameObject forwardMuzzleFlash;
+    private MuzzleFlash forwardMuzzleFlashScript;
+    [SerializeField]
+    private GameObject upMuzzleFlash;
+    private MuzzleFlash upMuzzleFlashScript;
+    [SerializeField]
+    private GameObject downMuzzleFlash;
+    private MuzzleFlash downMuzzleFlashScript;
 
     private ParticleSystem emitter;
+
 
     /// <summary>
     ///  Unity start.
@@ -22,11 +32,14 @@ public class MachineGun : BaseWeapon
         base.Start();
         inactiveBullets = new List<BaseBullet>();
         activeBullets = new List<BaseBullet>();
-        emitter = GetComponent<ParticleSystem>();
+        emitter = GetComponentInChildren<ParticleSystem>();
 
+        forwardMuzzleFlashScript = forwardMuzzleFlash.GetComponent<MuzzleFlash>();
+        upMuzzleFlashScript = upMuzzleFlash.GetComponent<MuzzleFlash>();
+        downMuzzleFlashScript = downMuzzleFlash.GetComponent<MuzzleFlash>();
 
         coolTime = WeaponsConstants.MachinGunCoolTime;
-        for(int i = 0; i < PoolItemsCount; i++)
+        for (int i = 0; i < PoolItemsCount; i++)
         {
             var bullet = Instantiate(bulletPrefab).GetComponent<MachineGunBullet>();
             bullet.gameObject.SetActive(false);
@@ -40,28 +53,34 @@ public class MachineGun : BaseWeapon
     /// </summary>
     public override void Shoot(Vector2 direction)
     {
-        if(canShoot == true)
+        if (canShoot == true)
         {
             coolTimer = 0;
             var bullet = inactiveBullets.First();
-            if(bullet != null)
+            if (bullet != null)
             {
                 inactiveBullets.RemoveAt(0);
                 activeBullets.Add(bullet);
 
                 var offset = BulletPositionOffset;
-                if(direction.x < 0)
+                var initialDirectionChanged = direction.x < 0;
+                if (initialDirectionChanged)
                 {
                     offset *= -1;
                 }
-            
+
                 bullet.gameObject.transform.position = gameObject.transform.position;
                 bullet.gameObject.transform.Translate(offset, 0, 0);
                 bullet.Direction = direction;
                 bullet.gameObject.SetActive(true);
                 emitter.Play();
+
+                forwardMuzzleFlashScript.Play();
+                upMuzzleFlashScript.Play(initialDirectionChanged);
+                downMuzzleFlashScript.Play(initialDirectionChanged);
             }
-            
         }
+
     }
 }
+
